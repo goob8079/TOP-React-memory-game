1a36e8afea9c4bff47e795b967ffb51e12148530
Justice <122542069+goob8079@users.noreply.github.com>
2025-11-16 14:42:58 +0900

    changed image paths

--- src/components/cardData.jsx
+++ src/components/cardData.jsx
@@ -1,52 +1,52 @@
 export const cardData = [
     { 
         id: 1,
-        img: 'src/assets/apple.jpg',
+        img: 'public/assets/apple.jpg',
         name: 'apple'
     },
     {
         id: 2,
-        img: 'src/assets/avocado.jpg',
+        img: 'public/assets/avocado.jpg',
         name: 'avocado'
     },
     {
         id: 3,
-        img: 'src/assets/banana.jpg',
+        img: 'public/assets/banana.jpg',
         name: 'banana'
     },
     {
         id: 4,
-        img: 'src/assets/blueberry.jpg',
+        img: 'public/assets/blueberry.jpg',
         name: 'blueberry'
     },
     {
         id: 5,
-        img: 'src/assets/cherry.jpg',
+        img: 'public/assets/cherry.jpg',
         name: 'cherry'
     },
     {
         id: 6,
-        img: 'src/assets/kiwi.jpg',
+        img: 'public/assets/kiwi.jpg',
         name: 'kiwi'
     },
     {
         id: 7,
-        img: 'src/assets/lemon.jpg',
+        img: 'public/assets/lemon.jpg',
         name: 'lemon'
     },
     {
         id: 8,
-        img: 'src/assets/orange.jpg',
+        img: 'public/assets/orange.jpg',
         name: 'orange'
     },
     {
         id: 9,
-        img: 'src/assets/pumpkin.jpg',
+        img: 'public/assets/pumpkin.jpg',
         name: 'pumpkin'
     },
     {
         id: 10,
-        img: 'src/assets/watermelon.jpg',
+        img: 'public/assets/watermelon.jpg',
         name: 'watermelon'
     }
 ]
